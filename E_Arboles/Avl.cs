66e61ed3a04e0b37097306401d414be8c9893ec6
J01Gonza <juanp01.gh@gmail.com>
2021-03-28 18:18:10 -0600

    Fix

--- E_Arboles/Avl.cs
+++ E_Arboles/Avl.cs
@@ -268,15 +268,6 @@ namespace E_Arboles
             Order += head.Key.ToString() + " =>";
             return Order;
         }
-
-        public void Delete(T delete)
-        {
-            Delete(Root, delete);
-        }
-        private Node Delete(Node root, T delete)
-        {
-            return 
-        }
     }
 }
 
